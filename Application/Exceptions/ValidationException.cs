6d5e467e3f1594d4f8c368ff2c1870d1dc238295
Hassan Almusawi <hassanzack@gmail.com>
2022-09-10 20:24:42 +0300

    Add

---
+++ Application/Exceptions/ValidationException.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using ApplicationException = Domain.Exceptions.ApplicationException;
+
+namespace Application.Exceptions
+{
+    public sealed class ValidationException : ApplicationException
+    {
+        public ValidationException(IReadOnlyDictionary<string, string[]> errorsDictionary)
+            : base("Validation Failure", "One or more validation errors occurred")
+            => ErrorsDictionary = errorsDictionary;
+
+        public IReadOnlyDictionary<string, string[]> ErrorsDictionary { get; }
+    }
+}
+/*
+API
+App
+Per
+Domen
+ 
+ 
+ */
---
+++ EventBus/EventBus/Abstractions/IDynamicIntegrationEventHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace EventBus.Abstractions
+{
+    public interface IDynamicIntegrationEventHandler
+    {
+        Task Handle(dynamic eventData);
+    }
+
+}
